641d29fa2cace351af4da1896ab9651194c481da
LaraEmiliaGB <lara_guzman@estudante.sesisenai.org.br>
2026-04-01 20:32:15 -0300

    add - funções

---
+++ calculadora.js
@@ -0,0 +1,70 @@
+function pegarValores {
+    // getElementById pega o input pelo id entre "", e .value pega o que foi digitado
+    let valor1 = document.getElementById("valor1").value; 
+    let valor2 = document.getElementById("valor2").value;
+    
+    // se algum campo estiver vazio return null para a função e devolve um valor vazio
+    if (valor1 === "" || valor2 === "") {
+        alert("Por favor preencha os dois campos!");
+        return null;
+    }
+
+    valor1 = parseFloat(valor1);
+    valor2 = parseFloat(valor2);
+
+    if (isNaN(valor1) || isNaN(valor2)) {
+        alert("Digite apenas valores válidos!");
+        return null; // para e devolve valor vazio se for Not a Number
+    }
+
+    return {valor1, valor2};
+}
+
+function somar() {
+    //chama a função que pega e valida os valores inseridos
+    let valores = pegarValores();
+
+    // se valores = null (vazio), para a função e não retorna nada
+    if (valores === null) return;
+
+    let resultado = valores.valo1 + valores.valor2;
+    mostrarResultado(resultado);
+}
+
+function subtrair() {
+    let valores = pegarValores();
+
+    if (valores === null) return;
+
+    let resultado = valores.valo1 - valores.valor2;
+    mostrarResultado(resultado);
+}
+
+function multiplicar() {
+    let valores = pegarValores();
+
+    if (valores === null) return;
+
+    let resultado = valores.valo1 * valores.valor2;
+    mostrarResultado(resultado);
+}
+
+function dividir() {
+    let valores = pegarValores();
+
+    if (valores === null) return;
+    
+    // se o segundo valor for zero
+    if (valor2 === 0) {
+        alert("Não é possível dividir por 0!");
+        return;
+    }
+
+    let resultado = valores.valo1 / valores.valor2;
+    mostrarResultado(resultado);
+}
+
+function mostrarResultado() {
+    // .innerText representa o texto visível do elemento
+    document.getElementById("result").innerText = "O resultado da operação é: " + resultado;
+}
